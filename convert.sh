80eecdeb683eefd10c6e53e073de8d2148089171
Tuomas Rossi <tuomas.rossi@csc.fi>
2023-04-04 14:12:36 +0000

    Define urls with --metadata-file

--- convert.sh
+++ convert.sh
@@ -30,7 +30,7 @@ theme_dpath="$SLIDEFACTORY_THEME_ROOT/$theme"
 for fpath in "$@"; do
     html_fpath=${fpath%.*}.html
     echo "Converting $fpath to $html_fpath"
-    pandoc -d "$theme_dpath/defaults.yaml" --template="$theme_dpath/template.html" -o "$html_fpath" "$theme_dpath/urls.yaml" "$fpath"
+    pandoc -d "$theme_dpath/defaults.yaml" --template="$theme_dpath/template.html" --metadata-file="$theme_dpath/urls_local.yaml" -o "$html_fpath" "$fpath"
     if [ $? -eq 0 ] && [ ! -z "$do_pdf" ]; then
         pdf_fpath=${fpath%.*}.pdf
         html_abs_fpath=$(readlink -f "$html_fpath")
